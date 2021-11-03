f30521716aeb2993dc8fa9f1a1169244fc14ea3d
João Henrique <joaohenriquetdj@gmail.com>
2021-11-03 20:29:34 -0300

    add Action delegate (ForEach)

--- DelegatesAndLambda/Program.cs
+++ DelegatesAndLambda/Program.cs
@@ -1,16 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using Delegates.Services;
+using Project01.Entities;
 
 namespace Delegates
 {
-     delegate double BinaryNumericOperation(double n1, double n2);
-     delegate void MultCastBinaryNumericOperation(double n1, double n2);
-    class Program
+    internal delegate double BinaryNumericOperation(double n1, double n2);
+
+    internal delegate void MultCastBinaryNumericOperation(double n1, double n2);
+
+    internal static class Program
     {
         public static void Main(string[] args)
         {
-            MulticastDelegate();
+            ActionDelegate();
+        }
 
+        
+        private static void ActionDelegate()
+        {
+            var products = new List<Product>
+            {
+                new Product {Id = 7, Name = "Hd Case", Price = 85.50},
+                new Product {Id = 1, Name = "Notebook", Price = 2580.97},
+                new Product {Id = 4, Name = "Tv", Price = 970.67},
+                new Product {Id = 2, Name = "Tablet", Price = 800.00}
+            };
+
+            products.ForEach(product => { product.Price += product.Price * 0.1; });
+            foreach (var product   in products) 
+            {
+                Console.WriteLine(product);
+            }
         }
 
         private static void MulticastDelegate()
@@ -21,6 +42,7 @@ namespace Delegates
             operation += CalculationService.ShowMax;
             operation(a, b);
         }
+
         private static void SumWithDelegates()
         {
             // Sum With Delegate
