39a506641d3e17daa79fb264ea0f19b9a21e7db3
João Henrique <joaohenriquetdj@gmail.com>
2021-11-03 15:18:49 -0300

    Add CalculationService

---
+++ Delegates/Services/CalculationService.cs
@@ -0,0 +1,20 @@
+﻿namespace Delegates.Services
+{
+    public class CalculationService
+    {
+        public static double Max(double n1, double n2)
+        {
+            return (n1 > n2) ? n1 : n2;
+        }
+
+        public static double Sum(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public static double Square(double value)
+        {
+            return value * value;
+        }
+    }
+}
