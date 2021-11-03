e509dfe9a8f6ec4c3a29d66137870410270bbcad
João Henrique <joaohenriquetdj@gmail.com>
2021-11-03 15:50:39 -0300

    add operations with MulticastDelegate

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,10 +4,26 @@ using Delegates.Services;
 namespace Delegates
 {
      delegate double BinaryNumericOperation(double n1, double n2);
+     delegate void MultCastBinaryNumericOperation(double n1, double n2);
     class Program
     {
         public static void Main(string[] args)
         {
+            MulticastDelegate();
+
+        }
+
+        private static void MulticastDelegate()
+        {
+            // Sum With Delegate
+            double a = 1, b = 92;
+            MultCastBinaryNumericOperation operation = CalculationService.ShowSum;
+            operation += CalculationService.ShowMax;
+            operation(a, b);
+        }
+        private static void SumWithDelegates()
+        {
+            // Sum With Delegate
             double a = 18, b = 92;
             BinaryNumericOperation operation = CalculationService.Sum;
             var result = operation(a, b);
--- Delegates/Services/CalculationService.cs
+++ Delegates/Services/CalculationService.cs
@@ -1,4 +1,6 @@
-﻿namespace Delegates.Services
+﻿using System;
+
+namespace Delegates.Services
 {
     public static class CalculationService
     {
@@ -7,14 +9,27 @@
             return (n1 > n2) ? n1 : n2;
         }
 
+        public static void ShowMax(double n1, double n2)
+        {
+            var max = Max(n1, n2);
+            Console.WriteLine(max);
+        }
+
         public static double Sum(double n1, double n2)
         {
             return n1 + n2;
+        }
+
+        public static void ShowSum(double n1, double n2)
+        {
+            var sum = Sum(n1, n2);
+            Console.WriteLine(sum);
         }
 
         public static double Square(double value)
         {
             return value * value;
         }
+        
     }
 }
