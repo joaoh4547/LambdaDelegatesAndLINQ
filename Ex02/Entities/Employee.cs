62f140741a0aff5c9272bc9fd1ec7f7012c4e608
João Henrique <joaohenriquetdj@gmail.com>
2021-11-04 15:13:18 -0300

    Finish Ex02

---
+++ Ex02/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Ex02.Entities
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Email: {Email}, Salary: {Salary.ToString("C", CultureInfo.GetCultureInfo("pt-br"))}";
+        }
+    }
+}
--- Ex02/Program.cs
+++ Ex02/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace Ex02
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Ex02.Entities;
+
+namespace Ex02
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var employees = new List<Employee>();
+            Console.Write("Enter full file path: ");
+            var path = Console.ReadLine();
+            Console.Write("Enter Salary: ");
+            var salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (File.Exists(path) && path != null)
+            {
+                using (StreamReader reader = File.OpenText(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var fields = reader.ReadLine().Split(',');
+
+                        employees.Add(new Employee(fields[0], fields[1],
+                            double.Parse(fields[2], CultureInfo.InvariantCulture)));
+                    }
+                }
+
+                var emails = employees.Where(e => e.Salary > salary).OrderBy(e => e.Email).Select(e => e.Email);
+                var salarySum = employees.Where(e => e.Name.StartsWith("M")).Sum(e => e.Salary);
+
+                Console.WriteLine(
+                    $"Email of people whose salary is more than {salary.ToString("C", CultureInfo.GetCultureInfo("pt-br"))} ");
+                foreach (var email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+
+                Console.WriteLine($"Sum of salary of people whose name starts with 'M': {salarySum.ToString("C", CultureInfo.GetCultureInfo("pt-br"))}");
+            }
+            else
+            {
+                throw new Exception();
+            }
         }
     }
 }
