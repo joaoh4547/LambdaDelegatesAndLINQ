29a00ef2151aa591ede17a0ac1d301e600b21679
João Henrique <joaohenriquetdj@gmail.com>
2021-11-03 21:27:29 -0300

    add LINQ Where and Select ToList

---
+++ LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            // Fonte de Dados
+            var numbers = new int[] {2, 3, 4, 5,6};
+            
+            
+            // expressão 
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10)
+                .ToList();
+            
+            // Executar a consulta 
+
+            foreach (var x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
